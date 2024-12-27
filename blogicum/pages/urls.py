b675207998db94fc0822aaa346a1865601cacab0
Рина Петрова <dopioxdiavolo@gmail.com>
2024-12-27 23:03:21 +0300

    FULL

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', TemplateView.as_view(template_name='template.html'),
+         name='about'),
+    path('rules/', TemplateView.as_view(template_name='template.html'),
+         name='rules')
+]
